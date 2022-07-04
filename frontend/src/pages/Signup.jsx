19a909f698891be94bd35b244ca5ff5c995ca7b4
Nico Pasqualini <pasqualinin9@gmail.com>
2022-07-04 16:45:45 +1000

    implementing cloudinary

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,21 +3,83 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
 import './Signup.css'
+import avatar from '../assets/avatar.png'
+import { useState } from 'react';
+
+
 function Signup() {
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  const [name,setName] = useState('')
+  const [image,setImage] = useState(null)
+  const [uploadingImg, setUploadingImg]=useState(false)
+  const [imgPreview, setImgPreview] = useState(null)
+
+  function validateImg(e){
+    const file = e.target.files[0]
+    // if size > 1 MB
+    if(file.size>1048576){
+      return alert('max file size is 1 MB ')
+    }
+    else{
+      setImage(file)
+      setImgPreview(URL.createObjectURL(file))
+    }
+  }
+
+  async function uploadImage(){
+    const data = new FormData()
+    data.append('file',image)
+    data.append('upload_preset','first_upload_preset')
+    try{
+      setUploadingImg(true)
+      // dmdaba3cq is the cloudinary name
+      let res = await fetch('https://api.cloudinary.com/v1_1/dmdaba3cq/image/upload',{
+        method:'post',
+        body:data
+      })
+      const urlData = await res.json()
+      setUploadingImg(false)
+      return urlData.url
+    }
+    catch(err){
+      setUploadingImg(false)
+      console.log(err)
+    }
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault()
+    if(!image){
+      return alert("Missing profile picture")
+    }
+    const url = await uploadImage(image)
+    console.log(url)
+  }
+  
+
   return (
     <Container>
       <Row>
         <Col md={4} className='signup__bg'></Col>
         <Col md ={8} className='d-flex align-items-center justify-content-center flex-direction-column'>
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <h1 className='text-center'>Create an account</h1>
+      <div className='signup-profile-pic-container'>
+        <img src={imgPreview ||avatar} className='image-signup' alt="profileImg"/>
+        <label htmlFor='image-upload' className='image-upload-label'>
+          <i className='fas fa-plus-circle add-picture-icon'></i>
+        </label>
+        {/* when we click we ope the folder with images to add a new one */}
+        <input type='file' id='image-upload' hidden accept='image/png, image/jpeg' onChange={validateImg} />
+      </div>
     <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Username</Form.Label>
-        <Form.Control type="text" placeholder="Username" />
+        <Form.Control type="text" placeholder="Username" onChange={(e)=>setName(e.target.value)} value={name}/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" onChange={(e)=>setEmail(e.target.value)} value={email} />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -25,7 +87,7 @@ function Signup() {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} value={password}/>
       </Form.Group>
      
       <Button variant="primary" type="submit">
